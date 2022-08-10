538ed15d0c096134b338929719d2f89af871410c
Ryan McGowan <vitomaserati@gmail.com>
2022-08-09 18:17:55 -0700

    new places can now be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,26 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'Pauls Pancakes',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Breakfast, Pancakes',
-    pic: '/images/pancakes.jpg'
-
-  }, {
-    name: 'Kikn Kabobs',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Meat, Kabobs',
-    pic: '/images/kabobs.jpg'
-  }]
-
-  res.render('places/index', { places })
+ res.render('places/index', { places })
 
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 
---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'Pauls Pancakes',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Breakfast, Pancakes',
+    pic: '/images/pancakes.jpg'
+
+  }, {
+    name: 'Kikn Kabobs',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Meat, Kabobs',
+    pic: '/images/kabobs.jpg'
+}]
