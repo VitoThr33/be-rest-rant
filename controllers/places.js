23d13229187d400e3f99d5aa95e34161456e999c
Ryan McGowan <vitomaserati@gmail.com>
2022-08-09 17:42:21 -0700

    spiced up the form page new place

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,29 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Pauls Pancakes',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Breakfast, Pancakes',
-        pic: '/images/pancakes.jpg'
-  
-      }, {
-        name: 'Kikn Kabobs',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Meat, Kabobs',
-        pic: '/images/kabobs.jpg'
-      }]
-      
-    res.render('places/index', {places})
-    
+  let places = [{
+    name: 'Pauls Pancakes',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Breakfast, Pancakes',
+    pic: '/images/pancakes.jpg'
+
+  }, {
+    name: 'Kikn Kabobs',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Meat, Kabobs',
+    pic: '/images/kabobs.jpg'
+  }]
+
+  res.render('places/index', { places })
+
 })
+
+router.post('/', (req, res) => {
+  res.send('POST /places')
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
