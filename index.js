9dd5249bd71f04acd9fd4b8c87f564f8d8509df5
Ryan McGowan <vitomaserati@gmail.com>
2022-08-10 19:59:51 -0700

    delete button working and edit takes to show page

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methOverride= require('method-override')
 
 //express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methOverride('_method'))
 
 //controllers/routes
 app.use('/places', require('./controllers/places'))
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,2 +1,15 @@
 const React = require('react')
 const Def = require('../default')
+
+function edit_form (data) {
+    return (
+        <Def>
+          <main>
+            <h1>Edit Place</h1>
+            
+          </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
