3766bf9a2f3d68f449978611e13a6f52cdd536ee
Ryan McGowan <vitomaserati@gmail.com>
2022-08-13 09:21:42 -0700

    add edit functionality

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //modules/globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
-const methOverride= require('method-override')
+
 
 //express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methOverride('_method'))
+app.use(methodOverride('_method'))
 
 //controllers/routes
 app.use('/places', require('./controllers/places'))
