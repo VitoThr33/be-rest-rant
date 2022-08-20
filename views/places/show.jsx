e76b4114bb6ad724768956a15d21c8fee888611e
Ryan McGowan <vitomaserati@gmail.com>
2022-08-20 16:24:54 -0700

    added comments section and functioning correctly

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,8 +2,27 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
-      <Def>
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
+    <Def>
       <main>
         <div className="row">
           <div className="col-sm-6">
@@ -29,27 +48,52 @@ function show (data) {
               Serving {data.place.cuisines}
             </h4>
             <br />
-            <h2>Rating</h2>
-            <p>No current rating</p>
-            <h3>Comments</h3>
-            <p>No comments yet</p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-  Edit
-</a>     
-
-<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-</div>
-</div>
-
-          </main>
-        </Def>
-    )
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>{` `}
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        <hr />
+        <h2>Comments</h2>
+        <div className="row">
+          {comments}
+        </div>
+        <hr />
+        <h2>Got Your Own Rant or Rave?</h2>
+        <form action={`/places/${data.place.id}/comment`} method="POST">
+          <div className="row">
+            <div className="form-group col-sm-12">
+              <label htmlFor="content">Content</label>
+              <textarea id="content" name="content" className="form-control"></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="form-group col-sm-4">
+              <label htmlFor="author">Author</label>
+              <input id="author" name="author" className="form-control" />
+            </div>
+            <div className="form-group col-sm-4">
+              <label htmlFor="stars">Star Rating</label>
+              <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+            </div>
+            <div className="form-group col-sm-2">
+              <label htmlFor="rant">Rant?</label>
+              <input type="checkbox" id="rant" name="rant" className="form-control" />
+            </div>
+          </div>
+          <input type="submit" className="btn btn-primary" value="Add Comment" />
+        </form>
+      </main>
+    </Def>
+)
 }
 
-module.exports =show
+module.exports = show
+
 
 
