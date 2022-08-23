6aa98c530cf82a7b58fcf4f90d07d11098302ede
Ryan McGowan <vitomaserati@gmail.com>
2022-08-22 18:49:54 -0700

    added star rating number functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,22 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    if (data.place.comments.length) {
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = sumRatings / data.place.comments.length
+      rating = (
+        <h3>
+          {Math.round(averageRating)} stars
+        </h3>
+      )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -20,7 +35,7 @@ function show (data) {
         </div>
       )
     })
-  }
+  }}
   return (
     <Def>
       <main>
@@ -36,7 +51,7 @@ function show (data) {
             <h2>
               Rating
             </h2>
-            
+            {rating}
             <br />
             <h2>
               Description
@@ -94,6 +109,5 @@ function show (data) {
 }
 
 module.exports = show
-
 
 
