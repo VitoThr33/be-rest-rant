89306fdc79e15483589348568561d92d2ccae61d
Ryan McGowan <vitomaserati@gmail.com>
2022-08-22 19:20:03 -0700

    fix edit and delete buttons

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -17,16 +17,20 @@ function show (data) {
       let sumRatings = data.place.comments.reduce((tot, c) => {
         return tot + c.stars
       }, 0)
-      let averageRating = sumRatings / data.place.comments.length
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i=0; i < averageRating; i++) {
+        stars += '⭐'
+      }
       rating = (
         <h3>
-          {Math.round(averageRating)} stars
+          {stars} stars
         </h3>
       )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h2 className="rant">{c.rant ? 'Rant! 🤬' : 'Rave! 😍'}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
